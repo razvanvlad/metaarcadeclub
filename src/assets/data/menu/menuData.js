5ce8577f4f98d554244d3aa10a52ab1b0a6dec5e
razvanvlad <razvanvladpop@gmail.com>
2023-03-09 20:16:31 +0200

    First commit

---
+++ src/assets/data/menu/menuData.js
@@ -0,0 +1,127 @@
+const data = [
+  {
+    id: "1W6WV",
+    title: "Home",
+    url: "/"
+  },
+  {
+    id: "2ZYYU",
+    title: "Arcade Hub",
+    url: "#",
+    subMenus: [
+      {
+        id: "0300T",
+        title: "Project Classic",
+        url: "/projects-classic-1",
+      },
+      {
+        id: "4XZ00",
+        title: "Project List",
+        url: "/projects-list",
+      },
+      {
+        id: "43X1V",
+        title: "Events Showcase",
+        url: "/projects-grid",
+      },
+      {
+        id: "4IX1V",
+        title: "Projects Calendar",
+        url: "/projects-calendar",
+      },
+      {
+        id: "VWXWY",
+        title: "Project Details",
+        url: "/projects-details-1",
+      },
+    ],
+  },
+  {
+    id: "ZZUVV",
+    title: "Pages",
+    url: "#",
+    subMenus: [
+      {
+        id: "5Z4XW",
+        title: "KYC Process",
+        url: "#",
+        subMenuChilds: [
+          {
+            id: "V6X1W",
+            title: "KYC Step 1",
+            url: "/kyc-1",
+          },
+          {
+            id: "1WXX4",
+            title: "KYC Step 2",
+            url: "/kyc-2",
+          },
+          {
+            id: "V6YVU",
+            title: "KYC Step 3",
+            url: "/kyc-3",
+          },
+        ],
+      },
+      {
+        id: "3YY6V",
+        title: "Auth Page",
+        url: "#",
+        subMenuChilds: [
+          {
+            id: "X6ZU4",
+            title: "Sign In",
+            url: "/login",
+          },
+          {
+            id: "YVWVW",
+            title: "Sign Up",
+            url: "/register",
+          },
+          {
+            id: "2ZYW6",
+            title: "Forget Password",
+            url: "/forget-password",
+          },
+        ],
+      },
+      {
+        id: "39Y6V",
+        title: "Farming",
+        url: "/farming",
+      },
+      {
+        id: "4XY6V",
+        title: "Leader Board",
+        url: "/leaderboard",
+      },
+      {
+        id: "4XP60",
+        title: "IGO Ranking",
+        url: "/igo-ranking",
+      },
+      {
+        id: "4XL90",
+        title: "IGO Apply",
+        url: "/igo-apply",
+      },
+      {
+        id: "PMQ60",
+        title: "Roadmap Details",
+        url: "/roadmap-details",
+      },
+      {
+        id: "39X8V",
+        title: "Team Details",
+        url: "/team-details",
+      },
+      {
+        id: "LTM20",
+        title: "Contact",
+        url: "/contact",
+      },
+    ],
+  },
+];
+
+export default data;
