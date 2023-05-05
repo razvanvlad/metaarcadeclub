944e5b1580464f4221b521bb13d93df0b1f505d8
razvanvlad <razvanvladpop@gmail.com>
2023-05-05 18:25:01 +0300

    game working

--- src/components/game/UnityGame.js
+++ src/components/game/UnityGame.js
@@ -1,32 +1,16 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 const UnityGame = () => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "/pong2/Build/Pong WebGL.loader.js";
-    script.async = true;
-    script.onload = () => {
-      if (typeof window.UnityLoader !== "undefined") {
-        window.UnityLoader.instantiate("unityContainer", "/pong/Build/Pong_WebGL.json", {
-          onProgress: (instance, progress) => {
-            console.log(`Loading: ${progress * 100}%`);
-          },
-        });
-      } else {
-        console.error("UnityLoader is not defined. Check if the loader script is loaded correctly.");
-      }
-    };
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
     <div>
       <h3>Loading Unity game...</h3>
-      <div id="unityContainer" style={{ width: "960px", height: "600px" }}></div>
+      <iframe
+        src="/game/game.html"
+        width="1920"
+        height="850"
+        style={{ border: "none" }}
+        title="Unity Game"
+      ></iframe>
     </div>
   );
 };
